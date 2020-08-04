ddb59a0418b28295ec139c37469cdaac005756a7
kajikasama <aguschandravii4@gmail.com>
2020-08-05 04:48:15 +0800

    add more features

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-// const token =  //isi sendiri di discord devnya / discordapp.com/devel
+// const token =  //isi sendiri di discord devnya / discordapp.com/developer
 
-const PREFIX = "!";
+const PREFIX = "~";
 
-let version = "v1.0.2";
+let version = "v1.0.3";
 
 bot.on("ready", () => {
-  console.log("This bot is onlen");
+  console.log("Bot ini sudah diaktifkan, Periksa Discord Anda");
 });
 
 bot.on("message", (message) => {
-  let args = message.content.substring(PREFIX.length).split(" ");
+  let args = message.content.substr(PREFIX.length).split(" ");
 
   switch (args[0]) {
     case "ping":
@@ -34,7 +34,7 @@ bot.on("message", (message) => {
         );
       } else {
         message.channel.send(
-          "Invalid Arguments Please Use *!help* To Specific Documentation Commands "
+          "Invalid Arguments Please Use Specific Documentation Commands "
         );
       }
       break;
@@ -42,6 +42,18 @@ bot.on("message", (message) => {
       if (!args[1])
         return message.reply("Error Please Define The Second Arguments");
       message.channel.bulkDelete(args[1]);
+      break;
+    case "embed":
+      const embed = new Discord.MessageEmbed()
+        .setTitle("User Information")
+        .addField("Player Name ", message.author.username, true)
+        .addField("Version", version, true)
+        .addField("Current Server", message.guild.name)
+        .setColor(0xff9142)
+        .setThumbnail(message.author.displayAvatarURL())
+        .setFooter("Feel Free To Add Me!");
+      message.channel.send(embed);
+
       break;
   }
 });
