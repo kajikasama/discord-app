0e94bd7771bb15472cf3573263dda8d4d2467aae
kajikasama <aguschandravii4@gmail.com>
2020-08-04 08:21:10 +0800

    add message ping, website, socialmedia, clear message, and version

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-// const token =  //isi sendiri
+// const token =  //isi sendiri di discord devnya / discordapp.com/devel
 
-bot.on('ready', () => {
-  console.log('This bot is onlen');
+const PREFIX = "!";
+
+let version = "v1.0.2";
+
+bot.on("ready", () => {
+  console.log("This bot is onlen");
 });
 
-bot.on('message', msg=> {
-  if(msg.content === 'Halo' || msg.content === 'halo' || msg.content === 'halo jacket' || msg.content === 'Halo Jacket')
-  {
-    msg.reply('Halo Gayn');
-  }
-});
-bot.on('message', msg=> {
-  if(msg.content === 'nama kamu siapa ?')
-  {
-    msg.reply(
-      'Perkenalkan, nama saya **Jacket** bisa juga '+
-      '**Jacket Girl** saya asli *prancis* kadang tinggal di *gensokyo*'+
-      ' hanya menjengunk ke-7 saudara saya yang ada disana ..'
-      );
+bot.on("message", (message) => {
+  let args = message.content.substring(PREFIX.length).split(" ");
+
+  switch (args[0]) {
+    case "ping":
+      message.channel.send("pong!");
+      break;
+    case "website":
+      message.channel.send("https://guschandra.epizy.com");
+      break;
+    case "info":
+      if (args[1] === "version") {
+        message.channel.send("Jacket Bot Version " + version);
+      } else if (args[1] === "socialmedia") {
+        message.channel.send("Facebook : https://fb.me/banana.garuda");
+        message.channel.send("Instagram : https://instagram.com/guschan12/");
+        message.channel.send("Twitter : https://twitter.com/KajikajiaSama");
+        message.channel.send("Github : https://github.com/kajikasama/");
+        message.channel.send(
+          "Youtube : https://youtube.com/channel/UC3YVyXSX2L1B3HHxCVBfCDw/"
+        );
+      } else {
+        message.channel.send(
+          "Invalid Arguments Please Use *!help* To Specific Documentation Commands "
+        );
+      }
+      break;
+    case "clear":
+      if (!args[1])
+        return message.reply("Error Please Define The Second Arguments");
+      message.channel.bulkDelete(args[1]);
+      break;
   }
 });
 
