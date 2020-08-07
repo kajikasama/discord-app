e06e5c4ab12dfc4f81adf2809003d7f38d22a1ac
kajikasama <aguschandravii4@gmail.com>
2020-08-07 09:38:07 +0800

    add dangerous features ; kick player

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
+const { Client, MessageAttachment, MessageEmbed } = require("discord.js"); //biasakan membuat sesuatu required dlu
+const bot = new Client(); //nulis bot biar kaga ribet make perintah discord.client lagi
 
-// const token =  //isi sendiri di discord devnya / discordapp.com/developer
+// const token =  //isi sendiri di discord devnya / discordapp.com/developerc
+const token = "NzM5ODQ1NzI0NDM4NzkwMjA0.XygZBg.vbopVJeYgJ2tz5A3tO75KLaGFgI";
 
+//character pemanggil botnya
 const PREFIX = "~";
 
-let version = "v1.0.4";
+//versi iseng doang tpi bisa aja sih ..
+let version = "v1.0.5";
 
+//ngecek bot ready di concole log / command prompt pilihan anda
 bot.on("ready", () => {
   console.log("Bot ini sudah diaktifkan, Periksa Discord Anda");
+  bot.user
+    .setActivity("Chandra Coding The Programs", { type: "WATCHING" })
+    .catch(console.error);
 });
 
+bot.on("guildMemberAdd", (member) => {
+  const channel = member.guild.channels.cache.find(
+    (channel) => channel.name === "welcum"
+  );
+  if (!channel) return;
+  channel.send(`Selamat datang di Indomaret, ${member}, Selamat Berbelanja ..`);
+});
+
+//buat pembangkit pesan jika terpanggil antara char dan kata yg disebutkan
 bot.on("message", (message) => {
   if (!message.content.startsWith(PREFIX)) return;
   // let args = message.content.substr(PREFIX.length).split(" ");
-  const args = message.content.slice(PREFIX.length).split(" ");
+  const args = message.content.slice(PREFIX.length).split(" "); //sambung char prefix dengan perintah yg ada
 
   switch (args[0]) {
-    case "ping":
+    case "ping": //ngetest pesan doang kaga ada yg penting sih
       message.channel.send("pong!");
       break;
-    case "website":
+    case "website": //nampilin web yg nggak pernah dilanjutin :"(
       message.channel.send("https://guschandra.epizy.com");
       break;
-    case "info":
+    case "info": //data diri saya ehh maksudnya social media saya hehe
       if (args[1] === "version") {
         message.channel.send("Jacket Bot Version " + version);
       } else if (args[1] === "socialmedia") {
@@ -35,6 +51,7 @@ bot.on("message", (message) => {
           "Youtube : https://youtube.com/channel/UC3YVyXSX2L1B3HHxCVBfCDw/"
         );
       } else {
+        //tulis secara spesific cuy jangan ngasal
         message.channel.send(
           "Invalid Arguments Please Use Specific Documentation Commands "
         );
@@ -42,11 +59,12 @@ bot.on("message", (message) => {
       break;
     case "clear":
       if (!args[1])
+        //kaga ada argumen pembantu ke-2
         return message.reply("Error Please Define The Second Arguments");
       message.channel.bulkDelete(args[1]);
       break;
-    case "embed":
-      const embed = new Discord.MessageEmbed()
+    case "embed": //manggil semua nama user, versi, hingga server yg dituju sang pemerintah
+      const embed = new MessageEmbed()
         .setTitle("User Information")
         .addField("Player Name ", message.author.username, true)
         .addField("Version", version, true)
@@ -56,17 +74,50 @@ bot.on("message", (message) => {
         .setFooter("Feel Free To Add Me!");
       message.channel.send(embed);
       break;
-    case "iloveyou":
+    case "iloveyou": //kau liat ini berarti anda geiii !!! jijjk aku cuk
       message.channel.send("Matane Cuk Jijik Ah ..");
       break;
-    case "i":
+    case "i": //kau liat ini berarti anda geiii !!! jijjk aku cuk cuman make spasi doang
       args[1] === "love"
         ? args[2] === "you"
           ? message.channel.send("Matane Cuk Jijik Ah `(*>﹏<*)′ ")
           : message.channel.send("Tolong Ngomong Di Perjelas ^_^ ..")
         : message.channel.send("Hmm ?? ..");
       break;
+    case "send":
+      const attachment = new MessageAttachment(
+        "https://danbooru.donmai.us/data/__jacket_girl_touhou_and_1_more_drawn_by_ebidashi_doragon7445__897ebf02a3e0c26701f91092dbfa7b7b.jpg"
+      );
+      message.channel.send(message.author, attachment);
+      break;
+    case "sendlocal":
+      const attachment2 = new MessageAttachment("./img/jacket-dipp.jpg");
+      message.channel.send(message.author, attachment2);
+      break;
+    case "rules":
+      const attachment3 = new MessageAttachment("./text/rules.txt");
+      message.channel.send(message.author, attachment3);
+      message.channel.send("Baca Sendiri Rulesnya Dalam Bentuk Dokumen");
+      break;
+    case "kick":
+      const user = message.mentions.users.first();
+      if (user) {
+        const member = message.guild.member(user);
+        if (member) {
+          member.kick("You where kick for trolling!").then(() => {
+            message.reply(`Successfully kicked ${user.tag}`).catch((err) => {
+              message.reply("I was unable to kick the member");
+              console.log(err);
+            });
+          });
+        } else {
+          message.reply(`That user isn't in the this world`);
+        }
+      } else {
+        message.channel.send("you need to specify a preson!");
+      }
+      break;
   }
 });
-
+//buat token utk connect botnya tanpa ini kaga bisa connect ke botnya
 bot.login(token);
---
+++ readme.md
@@ -0,0 +1,60 @@
+# **JacketGirl Bot Discord** 
+![Jacket Girl](/img/jacket-dipp.jpg)
+### Bot Discord Buatan sendiri
+
+<hr>
+
+## **Keenakan**
+
+- pake server sendiri biar nggak ngelagg
+- nggak butuh resource server lebih
+- bisa di add dimanapun
+- melatih programming 
+
+## **Kejelekan**
+- command tidak sesuai selera
+- beberapa fitur kudu dikoding ulang klo error
+- jalannya dikomputer sendiri klo **offline** iya bot juga **offline**
+
+<hr>
+
+## **Cara summonnya** :
+- download dulu 
+
+- install di folder lainnya
+
+- arahkan folder ke bash favorit klean
+
+- lalu ketik ini di terminal langganan anda
+```bash
+node .
+node index.js
+```
+
+>bilangin klo ada yg bug / error yg buat juga lagi mageran -_-
+
+## **Isi Commandnya**
+
+## version : `1.0.4`
+
+>biasakan klo mo manggil bot make tanta tilde `~`
+```
+~ping
+```
+
+| Command | Arti Perintahnya |
+| ------- | ---------------- |
+| `~ping` | ngetes ping dri server satuan ms (milisecond)|
+| `~website` | nampilin website yg buat bot ini asli buluk|
+| `~info version` | nampilin versi botnya|
+| `~info socialmedia` | nampilin social media yg buat bot nya asli ngemis ni *canda|
+|`~clear number input]` | hapus pesan sebanyak [ `tuliskan angka` ] |
+
+>dan lain sebagainya cari sendiri ..
+
+udah itu aja sih .. trima gaji ..
+
+## Koding By ~ **`Chandra`**
+![Chandra](/img/chandra.png)
+
+
